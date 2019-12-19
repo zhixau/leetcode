27fb61f49e3113d8dae5f7c9d0eb3b73884bc578
fangfang <wff820901@hotmail.com>
2019-12-19 22:23:30 +0800

    add rotate array solution 2

--- 189_rotate_array.py
+++ 189_rotate_array.py
@@ -14,3 +14,22 @@ class Solution(object):
         nums[:] = nums[-k:] + nums[:-k]
 
         return nums
+
+class Solution1(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        assert isinstance(nums, list)
+        len_array = len(nums)
+
+        if k > len_array:
+            k = k % len_array
+
+        for i in range(k):
+            nums.insert(0, nums[-1])
+            nums.pop()
+
+        return nums
